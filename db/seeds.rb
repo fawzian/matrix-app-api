0a657ae66fa4aec9b5be0c01a1b27f62135b8fea
FAWZIA NUR <FAWZIANUR@GMAIL.com>
2021-07-13 20:33:34 -0400

    Commit 9: Reset database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 Character.create([
-	{ name: "Neo", description: false },
-	{ name: "Trinity", description: false },
-	{ name: "Morpheus", description: false },
-	{ name: "Agent Smith", description: false },
+	{ name: "Neo", description: "1" },
+	{ name: "Trinity", description: "2" },
+	{ name: "Morpheus", description: "3" },
+	{ name: "Agent Smith", description: "4" },
 ])
